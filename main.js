8cb5b792be07107b817690cbc57e03d7cb5c65dc
JamesSwatton <jamessswatton@gmail.com>
2019-11-03 14:42:22 +0000

    add PrimarySchool and HighSchool classes

--- main.js
+++ main.js
@@ -24,6 +24,30 @@ class School {
         console.log(`${this.name} educates ${this.numberOfStudents} at the ${this.level} school level.`);
     }
     static pickSubstituteTeacher(substituteTeachers) {
-        return substituteTeachers[Math.floor(Math.random() * substituteTeachers.length)];
+        const randomIndex = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randomIndex];
     }
 }
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+    get sportsTeams() {
+        return this._sportsTeams;
+    }
+}
+
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13');
